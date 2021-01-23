ddeb4d04448c11651a2e2137dd9d46b238ef828e
Morten Joelsen <bonzai.slasher@gmail.com>
2021-01-23 19:46:40 +0100

    init

---
+++ BrawlhallaReader/Class1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Automation;
+using System.Windows.Automation.Text;
+
+namespace BrawlhallaReader
+{
+    public class Class1
+    {
+        private void dostuff()
+        {
+            Process proc = Process.GetProcessesByName("Brawlhalla").FirstOrDefault();
+            if (proc == null)
+                return;
+
+            AutomationElement element = AutomationElement.FromHandle(proc.MainWindowHandle);
+            AutomationElementCollection elements = null;
+            bool check = true;
+            int count = 0;
+            do
+            {
+                elements = element.FindAll(TreeScope.Descendants, Condition.TrueCondition);
+                if (elements != null)
+                {
+                    if (elements.Count > 0)
+                    {
+                        check = false;
+                    }
+                }
+                if (check)
+                {
+                    if (count > 100) // > 100 sec
+                    {
+                        return;
+                    }
+                    System.Threading.Thread.Sleep(1000);
+                    count++;
+                }
+            } while (check);
+
+            List<string> strings = new List<string>();
+
+            foreach (AutomationElement item in elements)
+            {
+                strings.Add(item.Current.Name);
+
+                Console.WriteLine(item.Current.Name);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ GoogleSheetsService/Class1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Sheets.v4;
+using Google.Apis.Sheets.v4.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System.IO;
+using System.Threading;
+using Data = Google.Apis.Sheets.v4.Data;
+
+namespace GoogleSheetsService
+{
+    public class Class1
+    {
+        private static void gtest()
+        {
+            string[] Scopes = { SheetsService.Scope.Spreadsheets };
+            string ApplicationName = "Brawlhalla Stats";
+
+            UserCredential credential;
+
+            using (var stream =
+                new FileStream("credentials.json", FileMode.Open, FileAccess.Read))
+            {
+                // The file token.json stores the user's access and refresh tokens, and is created
+                // automatically when the authorization flow completes for the first time.
+                string credPath = "token.json";
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+                Console.WriteLine("Credential file saved to: " + credPath);
+            }
+
+            // Create Google Sheets API service.
+            var service = new SheetsService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+        }
+
+        private static void test()
+        {
+            string[] Scopes = { SheetsService.Scope.Spreadsheets };
+            string ApplicationName = "Brawlhalla Stats";
+
+            UserCredential credential;
+
+            using (var stream =
+                new FileStream("credentials.json", FileMode.Open, FileAccess.Read))
+            {
+                // The file token.json stores the user's access and refresh tokens, and is created
+                // automatically when the authorization flow completes for the first time.
+                string credPath = "token.json";
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+                Console.WriteLine("Credential file saved to: " + credPath);
+            }
+
+            // Create Google Sheets API service.
+            var service = new SheetsService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+
+            // Define request parameters.
+            String spreadsheetId = "1MwKJaBCy4xRbT0hKpKGOr6vfZHmzhy4uJ76nfD78YWA";
+            String range = "Ark1!A2:E";
+            SpreadsheetsResource.ValuesResource.GetRequest request =
+                    service.Spreadsheets.Values.Get(spreadsheetId, range);
+
+
+            range = "Ark1!A2:Y";
+            string valueInputOption = "USER_ENTERED";
+            List<IList<object>> data = new List<IList<object>>();
+
+            // The new values to apply to the spreadsheet.
+            List<Data.ValueRange> updateData = new List<Data.ValueRange>();
+            var dataValueRange = new Data.ValueRange();
+            dataValueRange.Range = range;
+            dataValueRange.Values = data;
+            updateData.Add(dataValueRange);
+
+            Data.BatchUpdateValuesRequest requestBody = new Data.BatchUpdateValuesRequest();
+            requestBody.ValueInputOption = valueInputOption;
+            requestBody.Data = updateData;
+
+            //request = service.Spreadsheets.Values.BatchUpdate(requestBody, spreadsheetId);
+
+            service.Spreadsheets.Values.Append(null, spreadsheetId, range);
+
+            Console.Read();
+        }
+    }
+}
